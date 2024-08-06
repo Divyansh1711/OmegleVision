a1f10c3ccd92ebf1c998a11f05ac3657031d6b1e
divyansh <divyansh7404@gmail.com>
2024-08-07 02:19:36 +0530

    "Initial Commit"

---
+++ MatchingService/src/test/java/com/OmegleVision/MatchingService/MatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OmegleVision.MatchingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ WebRTCService/src/test/java/com/OmegleVision/WebRTCService/WebRtcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OmegleVision.WebRTCService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
